bdd597608039096d789a1c3e98382ae60b1c25bc
hans <divinity76@gmail.com>
2022-08-09 17:33:26 +0200

    add game compiler code
    compiles LegoRR with:
    - base game
    - d3drm.dll
    - DRM removed
    - dgVoodoo
    - no-cd-muic fix
    - llrce
    - mod manager cafeteria

--- src/compile.php
+++ src/compile.php
@@ -38,7 +38,7 @@ function unlinkRecursive(string $path, bool $verbose = false): void
             echo "unlink: {$path}\n";
         }
         if (!unlink($path)) {
-            throw new \RuntimeException("Failed to unlink {$path}: " . var_export(error_get_last(), true));
+            throw new \RuntimeException("Failed to unlink \"{$path}\": " . var_export(error_get_last(), true));
         }
         return;
     }
@@ -62,28 +62,28 @@ function unlinkRecursive(string $path, bool $verbose = false): void
     }
     foreach ($filesToDelete as $file) {
         if ($verbose) {
-            echo "unlink: {$file}\n";
+            echo "unlink: \"{$file}\"\n";
         }
         if (!unlink($file)) {
-            throw new \RuntimeException("Failed to unlink {$file}: " . var_export(error_get_last(), true));
+            throw new \RuntimeException("Failed to unlink \"{$file}\": " . var_export(error_get_last(), true));
         }
     }
     if ($verbose) {
         echo "rmdir: {$path}\n";
     }
     if (!rmdir($path)) {
-        throw new \RuntimeException("Failed to rmdir {$path}: " . var_export(error_get_last(), true));
+        throw new \RuntimeException("Failed to rmdir \"{$path}\": " . var_export(error_get_last(), true));
     }
 }
 
-function copyRecursive(string $source, string $destination, bool $verbose): void
+function copyRecursive(string $source, string $destination, bool $verbose = false): void
 {
     if (is_file($source)) {
         if ($verbose) {
-            echo "copy: {$source} -> {$destination}\n";
+            echo "copy: \"{$source}\" -> \"{$destination}\"\n";
         }
         if (!copy($source, $destination)) {
-            throw new \RuntimeException("Failed to copy {$source} to {$destination}: " . var_export(error_get_last(), true));
+            throw new \RuntimeException("Failed to copy \"{$source}\" to \"{$destination}\": " . var_export(error_get_last(), true));
         }
         return;
     }
@@ -92,7 +92,7 @@ function copyRecursive(string $source, string $destination, bool $verbose): void
             echo "mkdir: {$destination}\n";
         }
         if (!mkdir($destination)) {
-            throw new \RuntimeException("Failed to create folder {$destination}: " . var_export(error_get_last(), true));
+            throw new \RuntimeException("Failed to create folder \"{$destination}\": " . var_export(error_get_last(), true));
         }
     }
     $copyList = [];
@@ -105,7 +105,7 @@ function copyRecursive(string $source, string $destination, bool $verbose): void
         $copyList[] = array($sourcePath, $destinationPath);
     }
     foreach ($copyList as list($sourcePath, $destinationPath)) {
-        copyRecursive($sourcePath, $destinationPath);
+        copyRecursive($sourcePath, $destinationPath, $verbose);
     }
 }
 
@@ -120,23 +120,79 @@ class LegoRRCompiler
     function compile(): void
     {
         $build_dir = $this->build_dir =  __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "build_dir";
-        echo "clearing build_dir: {$build_dir}\n";
+        echo "clearing build_dir: \"{$build_dir}\"\n";
         unlinkRecursive($build_dir, true);
         if (!mkdir($build_dir, 0777, true)) {
-            throw new \RuntimeException("Failed to create build_dir: {$build_dir}");
+            throw new \RuntimeException("Failed to create build_dir: \"{$build_dir}\"");
         }
         $this->build_dir = realpath($build_dir);
         $this->assert_dir = realpath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "assets");
         assert(!!$this->asset_dir);
+        // sigh..
         $this->add_base_game();
+        $this->add_d3drm();
+        $this->remove_drm();
+        $this->add_dgvoodoo();
+        $this->add_music_fix();
+        $this->add_llrce();
+        $this->add_mod_manager_cafeteria();
+        echo "game compiled!\n";
     }
     private function add_base_game(): void
     {
         $build_dir = $this->build_dir;
-        $base_game_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "base_game";
-        echo "copying base_game: {$base_game_dir} to {$build_dir}\n";
-        $this->copy_dir($base_game_dir, $build_dir);
+        $base_game_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "base_game_with_drm";
+        echo "copying base_game: \"{$base_game_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($base_game_dir, $build_dir, true);
+    }
+    private function add_d3drm(): void
+    {
+        $build_dir = $this->build_dir;
+        $d3drm_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "d3drm";
+        echo "adding d3drm: \"{$d3drm_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($d3drm_dir, $build_dir, true);
+    }
+    private function remove_drm(): void
+    {
+        $build_dir = $this->build_dir;
+        $drm_free_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "LRR_Masterpiece_Editon_Executable_no_DRM";
+        echo "adding DRM-free files: \"{$drm_free_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($drm_free_dir, $build_dir, true);
+    }
+    private function add_music_fix(): void
+    {
+        $build_dir = $this->build_dir;
+        $musc_fix_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "LRR_music_fix_v1_1";
+        echo "adding music fix: \"{$musc_fix_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($musc_fix_dir, $build_dir, true);
+    }
+    private function add_dgvoodoo(): void
+    {
+        $build_dir = $this->build_dir;
+        $dgvoodoo_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "dgVoodoo2_79_1";
+        $tmp = $dgvoodoo_dir . DIRECTORY_SEPARATOR . "MS" . DIRECTORY_SEPARATOR . "x86";
+        echo "adding dgVoodoo: \"{$tmp}\" to \"{$build_dir}\"\n";
+        copyRecursive($tmp, $build_dir, true);
+        $tmp = $dgvoodoo_dir . DIRECTORY_SEPARATOR . "dgVoodoo.conf";
+        echo "adding dgVoodoo.conf: \"{$tmp}\" to \"{$build_dir}\"\n";
+        copyRecursive($tmp, $build_dir . DIRECTORY_SEPARATOR . "dgVoodoo.conf", true);
+        $tmp = $dgvoodoo_dir . DIRECTORY_SEPARATOR . "dgVoodooCpl.exe";
+        echo "adding dgVoodooCpl.exe: \"{$tmp}\" to \"{$build_dir}\"\n";
+        copyRecursive($tmp, $build_dir . DIRECTORY_SEPARATOR . "dgVoodooCpl.exe", true);
+    }
+    private function add_llrce(): void
+    {
+        $build_dir = $this->build_dir;
+        $lrr_ce_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "LLRCE_v1.0.3.1";
+        echo "adding LLRCE: \"{$lrr_ce_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($lrr_ce_dir, $build_dir, true);
+    }
+    private function add_mod_manager_cafeteria(): void
+    {
+        $build_dir = $this->build_dir;
+        $mm_cafeteria_dir = $this->assert_dir . DIRECTORY_SEPARATOR . "mod_manager_Cafeteria_v1.0BETA7";
+        echo "adding Mod_Manager_Cafeteria: \"{$mm_cafeteria_dir}\" to \"{$build_dir}\"\n";
+        copyRecursive($mm_cafeteria_dir, $build_dir, true);
     }
 }
-$compiler = new LegoRRCompiler();
-$compiler->compile();
+(new LegoRRCompiler())->compile();
